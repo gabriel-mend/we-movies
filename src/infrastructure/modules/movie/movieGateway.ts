6a3bc0607a862ceaa9f62f2168e8c40769b028d4
gabriel-mend <crowofcode@gmail.com>
2025-08-13 23:51:48 -0300

    fix: retirando either do gateway

--- src/infrastructure/modules/movie/movieGateway.ts
+++ src/infrastructure/modules/movie/movieGateway.ts
@@ -1,6 +1,5 @@
 import { Movie } from '@/business/domain/entities/movie'
-import { Either } from '@/infrastructure/shared/either'
 
 export interface MovieGateway {
-  findAll(): Promise<Either<Error, Movie[]>>
+  findAll(): Promise<Movie[] | null>
 }
--- src/infrastructure/modules/movie/movieGatewayImpl.ts
+++ src/infrastructure/modules/movie/movieGatewayImpl.ts
@@ -1,4 +1,3 @@
-import { Either, left, right } from '@/infrastructure/shared/either'
 import { MovieGateway } from './movieGateway'
 import { Movie } from '@/business/domain/entities/movie'
 import { HttpClient } from '@/infrastructure/external/http/http-client'
@@ -8,15 +7,15 @@ import { MovieDTO } from './dtos/movie-dto'
 export class MovieGatewayImpl implements MovieGateway {
   constructor(private readonly api: HttpClient) {}
 
-  async findAll(): Promise<Either<Error, Movie[]>> {
+  async findAll(): Promise<Movie[] | null> {
     try {
       const data = await this.api.get<MovieDTO>('movies')
       if (data.isLeft()) {
-        return left(new Error(data.value.message))
+        return null
       }
-      return right(MovieMapper.toDomain(data.value))
+      return MovieMapper.toDomain(data.value)
     } catch (error) {
-      return left(new Error('Ocorreu um na listagem dos filmes.'))
+      return null
     }
   }
 }

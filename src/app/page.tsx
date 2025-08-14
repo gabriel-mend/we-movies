a1a490c578aca6d0e3e752e15f676491a3bd1be0
gabriel-mend <crowofcode@gmail.com>
2025-08-14 00:26:16 -0300

    feat: adicionando Button do shadcn e criando componente de Empty

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 import { MovieFactory } from '@/infrastructure/factories/movie-factory'
+import { MoviesEmpty } from '@/presentation/screens/home/MoviesEmpty/MoviesEmpty'
 import { Page } from '@/presentation/shared/layout'
 
 export default async function Home() {
   const { movieGateway } = await MovieFactory()
   const movies = await movieGateway.findAll()
 
+  if (movies) {
+    return (
+      <Page>
+        <MoviesEmpty />
+      </Page>
+    )
+  }
+
   return (
     <Page>
-      <h1 className="text-2xl font-bold">Home Page</h1>
-      {movies.value.map((movie) => (
+      {/* <h1 className="text-2xl font-bold">Home Page</h1>
+      {movies.map((movie) => (
         <h1>{movie.title}</h1>
-      ))}
+      ))} */}
     </Page>
   )
 }
---
+++ src/presentation/screens/home/MoviesEmpty/MoviesEmpty.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/presentation/external/shadcn/ui/button'
+import reloadPersonIcon from '@public/icons/reload-person-icon.png'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function MoviesEmpty() {
+  return (
+    <div className="h-[516px] md:h-[596px] flex flex-col items-center w-full bg-white text-secondary rounded-sm py-16 p-2">
+      <h2 className="text-2xl font-bold text-center md:max-w-full max-w-[210px] leading-none">
+        Parece que não há nada por aqui :(
+      </h2>
+      <div className="flex flex-col items-center justify-center my-6  ">
+        <Image src={reloadPersonIcon} alt="" width={178} height={264} />
+        <div className="md:w-[447px] w-[173px] h-[1.36px] bg-[#3F3D56]"></div>
+      </div>
+      <Button
+        className="w-[173px] h-[40px] text-white font-bold text-xs cursor-pointer"
+        asChild
+      >
+        <Link href="/">Recarregar página</Link>
+      </Button>
+    </div>
+  )
+}

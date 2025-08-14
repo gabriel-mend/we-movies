b47c7bf266d072d09ef9ae1de90a4f3841cfc15d
gabriel-mend <crowofcode@gmail.com>
2025-08-14 15:10:53 -0300

    feat: adicionando tela de loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+import loadingIcon from '@public/icons/load-spinner.svg'
 export default function Loading() {
-  return <p>Loading...</p>
+  return (
+    <div className="flex justify-center p-10">
+      <Image
+        src={loadingIcon}
+        alt="Carregando..."
+        width={62}
+        height={62}
+        className="animate-spin"
+      />
+    </div>
+  )
 }

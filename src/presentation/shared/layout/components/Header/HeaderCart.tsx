642aefafaded57340990b8524273e517eeca89c9
gabriel-mend <crowofcode@gmail.com>
2025-08-14 01:29:55 -0300

    responsive: ocultando titulo do carrinho no mobile

--- src/presentation/shared/layout/components/Header/HeaderCart.tsx
+++ src/presentation/shared/layout/components/Header/HeaderCart.tsx
@@ -5,7 +5,7 @@ export function HeaderCart() {
   return (
     <div className="flex gap-2">
       <div className="flex flex-col justify-center items-end">
-        <p className="text-sm font-semibold">Meu Carrinho</p>
+        <p className="text-sm font-semibold sm:flex hidden">Meu Carrinho</p>
         <span className="text-xs text-gray-regular">0 itens</span>
       </div>
       <Image src={cartIcon} alt="Carrinho" width={40} height={40} />
